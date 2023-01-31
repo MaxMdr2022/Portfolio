7c242b1e123acecbc91a28b2e4f14410df326c6a
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-01-30 21:31:57 -0300

    formularioDeEnvioPHP

--- enviar.php
+++ enviar.php
@@ -1,11 +1,22 @@
 <?php
-    $destino= "maxi_meder94@hotmail.com";
-    $nombre= $_POST["name"];
-    $correo= $_POST["email"];
-    $mensaje= $_POST["message"];
+$name = $_POST['name'];
+$mail = $_POST['email'];
+$message = $_POST['message'];
 
-    $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nMensaje: " . $mensaje;
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
 
-    mail($destino, "Contacto Portfolio", $contenido);
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
 
+$para = 'maxi_meder94@hotmail.com';
+$asunto = 'Mensaje portfolio';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:index.html");
 ?>
